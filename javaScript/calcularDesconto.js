9136b59ef92e59a4d76d28a66fcc2b32d8a97c9d
Calebe Carvalho <calebecvsts@gmail.com>
2025-07-12 18:50:40 -0300

    edição

--- javaScript/calcularDesconto.js
+++ javaScript/calcularDesconto.js
@@ -1,10 +1,10 @@
 export function desconto(pessoas) {
-
     pessoas.forEach(pessoa => {
-        if (pessoa.opcaoVT == true) {
+        console.log("Pessoa analisada:", pessoa.nome, pessoa.salario, pessoa.opcaoVT);
+
+        if (pessoa.opcaoVT) {
             const salario = pessoa.salario;
             pessoa.desconto = (salario * 0.06).toFixed(2);
-
         } else {
             pessoa.desconto = `Não optou pelo VT`;
         }
